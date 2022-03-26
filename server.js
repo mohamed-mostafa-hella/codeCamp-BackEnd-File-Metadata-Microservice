613546112d805c5c89bfd540d90d8b16f5f744d2
unknown <hoomos98@gmail.com>
2022-03-26 23:44:21 +0200

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const req = require('express/lib/request');
 require('dotenv').config()
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -10,6 +14,21 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  req.file.filename =  req.file.originalname;
+  console.log(req.file ,  req.body);
+  res.json({
+    name: req.file.originalname,
+    type : req.file.mimetype,
+    size : req.file.size
+  });
+  res.status(200);
+  res.end();
+})
 
 
 
